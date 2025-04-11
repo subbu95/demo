eb5b9e14922159e4057e2527b283faadca18ad01
subbu95 <40350341+subbu95@users.noreply.github.com>
2025-04-11 16:55:08 +0530

    Create links.ts

---
+++ src/app/links.ts
@@ -0,0 +1,13 @@
+https://www.figma.com/design/DTlOgdxKSBzjDXKuxte8GP/ECLIPSE-NIQ?node-id=2577-48755&t=1BtkzbyHijmvQ2og-1
+
+
+
+https://www.figma.com/design/NLv3vRWu8JlHUzmbLxnTSP/POC-reDesign?node-id=4442-414875&t=43zkNxzFDSj5UJ0Q-1
+
+
+
+
+https://www.figma.com/design/NLv3vRWu8JlHUzmbLxnTSP/POC-reDesign?node-id=1010-168342&t=43zkNxzFDSj5UJ0Q-1
+
+
+https://www.figma.com/design/NLv3vRWu8JlHUzmbLxnTSP/POC-reDesign?node-id=4432-402329&t=43zkNxzFDSj5UJ0Q-1
