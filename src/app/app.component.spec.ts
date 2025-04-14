39b213bf5799a2817391e115d7640d3ec72cef62
subbu95 <40350341+subbu95@users.noreply.github.com>
2025-04-14 22:36:22 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,61 +7,83 @@ describe('AppComponent', () => {
       imports: [AppComponent],
     }).compileComponents();
   });
-Error: src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:15:67 - error TS2344: Type '"getDetailsData"' does not satisfy the constraint 'keyof DetailsDataService'.
-
-15   let mockDetailsService: jasmine.SpyObj<Pick<DetailsDataService, 'getDetailsData'>>;
-                                                                     ~~~~~~~~~~~~~~~~
-
-
-Error: src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:17:63 - error TS2344: Type '"downloadFile"' does not satisfy the constraint 'keyof UtilityService'.
-
-17   let mockUtilityService: jasmine.SpyObj<Pick<UtilityService, 'downloadFile'>>;
-                                                                 ~~~~~~~~~~~~~~
-
-
-Error: src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:46:5 - error TS18046: 'mockDetailsService.getDetailsData' is of type 'unknown'.
-
-46     mockDetailsService.getDetailsData.and.returnValue(of([{ label: 'Status', value: 'OK' }]));
-       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-Error: src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:47:15 - error TS2339: Property 'ngOnInit' does not exist on type 'EclipseAthenaDialogComponent'.
-
-47     component.ngOnInit();
-                 ~~~~~~~~
-
-
-Error: src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:59:5 - error TS18046: 'mockDetailsService.getDetailsData' is of type 'unknown'.
-
-59     mockDetailsService.getDetailsData.and.returnValue(of(mockData));
-       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-Error: src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:69:5 - error TS18046: 'mockDetailsService.getDetailsData' is of type 'unknown'.
-
-69     mockDetailsService.getDetailsData.and.returnValue(throwError(() => new Error('Failure')));
-       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-Error: src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:76:21 - error TS2339: Property 'closeDialog' does not exist on type 'EclipseAthenaDialogComponent'.
-
-76     spyOn(component.closeDialog, 'emit');
-                       ~~~~~~~~~~~
-
-
-Error: src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:78:22 - error TS2339: Property 'closeDialog' does not exist on type 'EclipseAthenaDialogComponent'.
-
-78     expect(component.closeDialog.emit).toHaveBeenCalled();
-                        ~~~~~~~~~~~
-
-
-Error: src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:82:21 - error TS2339: Property 'closeDialog' does not exist on type 'EclipseAthenaDialogComponent'.
-
-82     spyOn(component.closeDialog, 'emit');
-                       ~~~~~~~~~~~
-
-
-Error: src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:84:22 - error TS2339: Property 'closeDialog' does not exist on type 'EclipseAthenaDialogComponent'.
-
-84     expect(component.closeDialog.emit).toHaveBeenCalled();
+LOG: Error: Test Error
+Error: Test Error
+    at errorFactory (http://localhost:9876/_karma_webpack_/webpack:/src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:348:72)
+    at Observable._subscribe (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/observable/throwError.js:5:51)
+    at Observable._trySubscribe (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:37:25)
+    at cb (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:31:30)
+    at errorContext (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/util/errorContext.js:19:9)
+    at Observable.subscribe (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:22:21)
+    at EclipseAthenaDialogComponent.downloadFile (http://localhost:9876/_karma_webpack_/webpack:/src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.ts:196:94)
+    at UserContext.apply (http://localhost:9876/_karma_webpack_/webpack:/src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:350:17)
+    at UserContext.apply (http://localhost:9876/_karma_webpack_/webpack:/node_modules/zone.js/fesm2015/zone-testing.js:1702:26)
+    at _ZoneDelegate.invoke (http://localhost:9876/_karma_webpack_/webpack:/node_modules/zone.js/fesm2015/zone.js:369:28)
+Chrome Headless 131.0.0.0 (Windows 10): Executed 29 of 126 SUCCESS (0 secs / 1.304 secs)
+LOG: Error: Test Error
+Error: Test Error
+    at errorFactory (http://localhost:9876/_karma_webpack_/webpack:/src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:348:72)
+    at Observable._subscribe (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/observable/throwError.js:5:51)
+    at Observable._trySubscribe (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:37:25)
+    at cb (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:31:30)
+    at errorContext (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/util/errorContext.js:19:9)
+    at Observable.subscribe (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:22:21)
+    at EclipseAthenaDialogComponent.downloadFile (http://localhost:9876/_karma_webpack_/webpack:/src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.ts:196:94)
+    at UserContext.apply (http://localhost:9876/_karma_webpack_/webpack:/src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:350:17)
+    at UserContext.apply (http://localhost:9876/_karma_webpack_/webpack:/node_modules/zone.js/fesm2015/zone-testing.js:1702:26)
+Chrome Headless 131.0.0.0 (Windows 10) EclipseAthenaDialogComponent downloadFile should download file successfully FAILED
+        Expected spy saveAs to have been called.
+            at <Jasmine>
+            at UserContext.apply (src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:318:36)
+            at UserContext.apply (node_modules/zone.js/fesm2015/zone-testing.js:1702:26)
+            at _ZoneDelegate.invoke (node_modules/zone.js/fesm2015/zone.js:369:28)
+            at ProxyZoneSpec.onInvoke (node_modules/zone.js/fesm2015/zone-testing.js:2081:39)
+LOG: Object{error: Object{error: 'Download failed'}}
+Chrome Headless 131.0.0.0 (Windows 10): Executed 31 of 126 (1 FAILED) (0 secs / 1.337 secs)
+Chrome Headless 131.0.0.0 (Windows 10) EclipseAthenaDialogComponent should initialize with correct default values FAILED
+        Expected undefined to be 'loader-image-path'.
+            at <Jasmine>
+            at UserContext.apply (src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:130:38)
+            at _ZoneDelegate.invoke (node_modules/zone.js/fesm2015/zone.js:369:28)
+            at ProxyZoneSpec.onInvoke (node_modules/zone.js/fesm2015/zone-testing.js:2081:39)
+            at _ZoneDelegate.invoke (node_modules/zone.js/fesm2015/zone.js:368:34)
+LOG: Object{error: Object{status: Object{errors: ..., responseStatus: ...}}}
+Chrome Headless 131.0.0.0 (Windows 10): Executed 36 of 126 (2 FAILED) (0 secs / 1.399 secs)
+LOG: Error: Test Error
+Error: Test Error
+    at errorFactory (http://localhost:9876/_karma_webpack_/webpack:/src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:298:76)
+    at Observable._subscribe (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/observable/throwError.js:5:51)
+    at Observable._trySubscribe (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:37:25)
+    at cb (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:31:30)
+    at errorContext (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/util/errorContext.js:19:9)
+    at Observable.subscribe (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:22:21)
+    at init (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/operators/finalize.js:5:20)
+    at SafeSubscriber.call (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/util/lift.js:10:28)
+    at cb (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:26:30)
+    at errorContext (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/util/errorContext.js:19:9)
+Chrome Headless 131.0.0.0 (Windows 10): Executed 39 of 126 (2 FAILED) (0 secs / 1.442 secs)
+LOG: Error: Test Error
+Error: Test Error
+    at errorFactory (http://localhost:9876/_karma_webpack_/webpack:/src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:298:76)
+    at Observable._subscribe (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/observable/throwError.js:5:51)
+    at Observable._trySubscribe (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:37:25)
+    at cb (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:31:30)
+    at errorContext (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/util/errorContext.js:19:9)
+    at Observable.subscribe (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:22:21)
+    at init (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/operators/finalize.js:5:20)
+    at SafeSubscriber.call (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/util/lift.js:10:28)
+    at cb (http://localhost:9876/_karma_webpack_/webpack:/node_modules/rxjs/dist/esm/internal/Observable.js:26:30)
+LOG: Object{error: Object{status: Object{errors: ..., responseStatus: ...}}}
+Chrome Headless 131.0.0.0 (Windows 10): Executed 40 of 126 (2 FAILED) (0 secs / 1.455 secs)
+Chrome Headless 131.0.0.0 (Windows 10) EclipseAthenaDialogComponent openDialog should open dialog and load job log data FAILED
+        Expected false to be true.
+            at <Jasmine>
+            at UserContext.apply (src/app/shared/components/eclipse-athena-dialog/eclipse-athena-dialog.component.spec.ts:139:35)
+            at _ZoneDelegate.invoke (node_modules/zone.js/fesm2015/zone.js:369:28)
+            at ProxyZoneSpec.onInvoke (node_modules/zone.js/fesm2015/zone-testing.js:2081:39)
+            at _ZoneDelegate.invoke (node_modules/zone.js/fesm2015/zone.js:368:34)
+WARN: 'Spec 'IframeTrackerDirective should create an instance' has no expectations.'
+Chrome Headless 131.0.0.0 (Windows 10): Executed 85 of 126 (3 FAILED) (0 secs / 2.397 secs)
+Chrome Headless 131.0.0.0 (Windows 10): Executed 126 of 126 (3 FAILED) (3.302 secs / 2.794 secs)
+TOTAL: 3 FAILED, 123 SUCCESS
 });
